e7a35e68954f290ae9d883ae94ae1bb9a59466b9
abin c joseph <abincjoseph77@gmail.com>
2024-07-17 21:07:18 +0530

    products_priority_is_added_and_home_page_products_is_activated

--- shopkart/shopkart/product/views.py
+++ shopkart/shopkart/product/views.py
@@ -3,9 +3,15 @@ from .models import Product
 from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    Featured_Products = Product.objects.order_by('priority')[:4] #[:4]is indicating first 4product in the priority list
+    Latest_Products = Product.objects.order_by('-id')[:4] #(-id) is indicate letest id in the database
+    context = {
+        'Featured_Products':Featured_Products,
+        'Latest_Products':Latest_Products
+    }
+    return render(request,'index.html',context)
 
-def list_product(request):
+def list_product(request,pk):
     """_summary_
     returns product list page
 
@@ -18,7 +24,7 @@ def list_product(request):
     page = 1
     if request.GET:
         page=request.GET.get('page',1)
-    product_list=Product.objects.all()
+    product_list=Product.objects.order_by('-priority') #order by is inticated to data sorted by the argument pat-24 14.03
     product_paginator = Paginator(product_list,4) #paginator objects passing for how may prodduct listing a single page
     product_list=product_paginator.get_page(page)  #part-23 8.54
     context={'products':product_list}
